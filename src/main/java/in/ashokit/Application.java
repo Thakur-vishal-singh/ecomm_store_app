c51b728e2895c63e2e77d9c4b65204853e111ffa
Thakur-vishal-singh <167334343+Thakur-vishal-singh@users.noreply.github.com>
2024-12-02 03:37:43 -0500

    Update Application.java

--- src/main/java/in/ashokit/Application.java
+++ src/main/java/in/ashokit/Application.java
@@ -13,8 +13,8 @@ public class Application {
                 System.out.println(i);
                    int k = 30;
                 System.out.println(j);
-		char gender = 'M';
-                int age = 20;
+		boolean isSaved = false;
+		boolean isUpdate = true;
                 String name = "ashokit";
                 double  price = 123.90;
 		
